ae7b3df75455306b82710c4d0828423127446206
Aliaksei Kolesau <kolesov93@gmail.com>
2020-04-20 15:56:18 +0300

    Add stub for main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.8
+import argparse
+import torch
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    return parser.parse_args()
+
+def main(args):
+    pass
+
+
+if __name__ == '__main__':
+    main(_parse_args())
