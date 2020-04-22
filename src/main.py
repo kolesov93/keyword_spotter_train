0f6852d726a75445bde864928525f5eaf4239575
Aliaksei Kolesau <kolesov93@gmail.com>
2020-04-22 12:36:00 +0300

    Add resnet model from honk

--- src/main.py
+++ src/main.py
@@ -2,11 +2,20 @@
 """Train keyword spotter."""
 import argparse
 import logging
+import copy
 
-import sounddevice as sd
+import numpy as np
+import torch
+import torch.utils.data
+import torch.nn as nn
+import torchaudio
+from torch.autograd import Variable
+import torch.nn.functional as F
 
 import data.google_speech_commands as gsc
 from data.common import DatasetTag
+
+import models.resnet as resnet
 
 LOGGER = logging.getLogger('spotter_train')
 
@@ -15,11 +24,144 @@ TEST_PERCENTAGE = 10.
 
 WANTED_WORDS = 'yes,no,up,down,left,right,on,off,stop,go'.split(',')
 
+BATCH_SIZE = 32
+LR = 1e-2
+DEV_EVERY_BATCHES = 1024
+
+def collate_fn(samples):
+    x, y = [], []
+    for sample in samples:
+        x.append(
+            torchaudio.compliance.kaldi.fbank(
+                torch.from_numpy(sample['data'].reshape(1, -1)),
+                num_mel_bins=80,
+                dither=0.
+            ).reshape(1, -1, 80)
+        )
+        y.append(sample['label'])
+    x = torch.cat(x, 0)
+    y = np.array(y, dtype=np.int64)
+    return x, torch.from_numpy(y)
 
 def _parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('data', help='path/to/google_speech_command/dataset')
     return parser.parse_args()
+
+class LinearModel(torch.nn.Module):
+
+    def __init__(self, D_in, D_out):
+        super(LinearModel, self).__init__()
+        self.linear = torch.nn.Linear(D_in, D_out)
+
+    def forward(self, x):
+        y = self.linear(x)
+        return y
+
+class FeedforwardModel(torch.nn.Module):
+
+    def __init__(self, units, noutputs):
+        super(FeedforwardModel, self).__init__()
+        timestamps = 98
+        prev = 80
+        self._linears = []
+        for unit in units:
+            self._linears.append(torch.nn.Linear(prev, unit))
+            prev = unit
+        self._final = torch.nn.Linear(prev * timestamps, noutputs)
+
+    def forward(self, x):
+        batch_size = x.shape[0]
+
+        xs = []
+        for curx in x:
+            xs.append(
+                torchaudio.compliance.kaldi.fbank(curx.reshape(1, -1), num_mel_bins=80, dither=0.)
+            )
+        x = torch.cat(xs, 0).reshape(batch_size, -1, 80)
+
+        for linear in self._linears:
+            x = linear(x)
+            x = F.relu(x)
+
+        x = x.reshape(x.shape[0], -1)
+        x = self._final(x)
+        return x
+
+
+def evaluate(model, dataset):
+    LOGGER.info('Starting evaluation')
+
+    loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=32,
+        collate_fn=collate_fn
+    )
+    model.eval()
+    loss_sum = 0.
+    nbatches = 0
+    criterion = nn.CrossEntropyLoss()
+    for i, (x, y) in enumerate(loader):
+        scores = model(x)
+        loss = criterion(scores, y)
+        loss_sum += loss.item()
+        nbatches += 1
+
+        if nbatches % 10 == 0:
+            LOGGER.info('Evaluation-xent [batch: %d]: %.02f', nbatches, loss_sum / nbatches)
+
+    result_loss = loss_sum / nbatches
+    LOGGER.info('Evaluation-xent: %.02f', result_loss)
+    return result_loss
+
+def train(args, sets):
+    #model = FeedforwardModel([128, 128, 64], len(WANTED_WORDS) + 2)
+    #model = LinearModel(16000, len(WANTED_WORDS) + 2)
+    config = copy.deepcopy(resnet.RES8_CONFIG)
+    config['n_labels'] = len(WANTED_WORDS) + 2
+    model = resnet.SpeechResModel(config)
+
+    prev_eval_loss = evaluate(model, sets[DatasetTag.DEV])
+
+    loader = torch.utils.data.DataLoader(
+        sets[DatasetTag.TRAIN],
+        batch_size=BATCH_SIZE,
+        collate_fn=collate_fn,
+        num_workers=4
+    )
+
+    criterion = nn.CrossEntropyLoss()
+    curlr = LR
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        lr=curlr,
+    )
+
+    for batch_idx, (x, y) in enumerate(loader):
+        model.train()
+        optimizer.zero_grad()
+        x = Variable(x, requires_grad=False)
+        scores = model(x)
+        y = Variable(y, requires_grad=False)
+        loss = criterion(scores, y)
+        loss.backward()
+        optimizer.step()
+
+        if batch_idx % 10 == 0:
+            LOGGER.info('Train-xent [batch %d]: %.02f', batch_idx, loss.item())
+
+        if (batch_idx + 1) % DEV_EVERY_BATCHES == 0:
+            new_eval_loss = evaluate(model, sets[DatasetTag.DEV])
+            if new_eval_loss > prev_eval_loss:
+                LOGGER.info('Evaluation loss (%.02f) is bigger than previous (%.02f)', new_eval_loss, prev_eval_loss)
+                curlr = curlr / 1.5
+                optimizer = torch.optim.SGD(
+                    model.parameters(),
+                    lr=curlr,
+                )
+            else:
+                LOGGER.info('Evaluation loss (%.02f) is less than previous (%.02f); continue', new_eval_loss, prev_eval_loss)
+                prev_eval_loss = new_eval_loss
 
 
 def main(args):
@@ -27,14 +169,17 @@ def main(args):
     LOGGER.setLevel(logging.INFO)
 
     indexes = gsc.split_index(gsc.get_index(args.data), DEV_PERCENTAGE, TEST_PERCENTAGE)
-    trainset = gsc.GoogleSpeechCommandsDataset(
-        indexes[DatasetTag.TRAIN],
-        WANTED_WORDS,
-        DatasetTag.TRAIN
-    )
-    for sample in trainset:
-        print(gsc._get_label(sample['label'], WANTED_WORDS))
-        # sd.play(sample['data'], gsc.SAMPLE_RATE, blocking=True)
+    sets = [
+        gsc.GoogleSpeechCommandsDataset(
+            indexes[tag],
+            WANTED_WORDS,
+            tag
+        )
+        for tag in DatasetTag
+    ]
+
+    train(args, sets)
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     main(_parse_args())
---
+++ src/models/resnet.py
@@ -0,0 +1,56 @@
+import torch.nn as nn
+import torch
+
+RES8_CONFIG = dict(n_labels=12, n_layers=6, n_feature_maps=45, res_pool=(4, 3), use_dilation=False)
+import torch.nn.functional as F
+
+class SerializableModule(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def save(self, filename):
+        torch.save(self.state_dict(), filename)
+
+    def load(self, filename):
+        self.load_state_dict(torch.load(filename, map_location=lambda storage, loc: storage))
+
+class SpeechResModel(SerializableModule):
+    def __init__(self, config):
+        super().__init__()
+        n_labels = config["n_labels"]
+        n_maps = config["n_feature_maps"]
+        self.conv0 = nn.Conv2d(1, n_maps, (3, 3), padding=(1, 1), bias=False)
+        if "res_pool" in config:
+            self.pool = nn.AvgPool2d(config["res_pool"])
+
+        self.n_layers = n_layers = config["n_layers"]
+        dilation = config["use_dilation"]
+        if dilation:
+            self.convs = [nn.Conv2d(n_maps, n_maps, (3, 3), padding=int(2**(i // 3)), dilation=int(2**(i // 3)),
+                bias=False) for i in range(n_layers)]
+        else:
+            self.convs = [nn.Conv2d(n_maps, n_maps, (3, 3), padding=1, dilation=1,
+                bias=False) for _ in range(n_layers)]
+        for i, conv in enumerate(self.convs):
+            self.add_module("bn{}".format(i + 1), nn.BatchNorm2d(n_maps, affine=False))
+            self.add_module("conv{}".format(i + 1), conv)
+        self.output = nn.Linear(n_maps, n_labels)
+
+    def forward(self, x):
+        x = x.unsqueeze(1)
+        for i in range(self.n_layers + 1):
+            y = F.relu(getattr(self, "conv{}".format(i))(x))
+            if i == 0:
+                if hasattr(self, "pool"):
+                    y = self.pool(y)
+                old_x = y
+            if i > 0 and i % 2 == 0:
+                x = y + old_x
+                old_x = x
+            else:
+                x = y
+            if i > 0:
+                x = getattr(self, "bn{}".format(i))(x)
+        x = x.view(x.size(0), x.size(1), -1) # shape: (batch, feats, o3)
+        x = torch.mean(x, 2)
+        return self.output(x)
