678937e11fa45de7fe1fc3200e9f6edd22422827
Aliaksei Kolesau <kolesov93@voice-iva.sas.yp-c.yandex.net>
2021-03-02 13:54:06 +0300

    Uptade parse_results.py

--- src/parse_results.py
+++ src/parse_results.py
@@ -4,7 +4,104 @@ import json
 import sys
 import os
 
-NEEDED = ['limit', 'use_fbank', 'batch_size', 'dev_every_batches', 'model', 'lr', 'lr_drop', 'data']
+class FieldGetter:
+
+    def __init__(self, field):
+        self._field = field
+
+    def get_value(self, options):
+        return options[self._field]
+
+    def get_formatted_value(self, options):
+        return str(self.get_value(options))
+
+
+class LangGetter:
+
+    def get_value(self, options):
+        data = options['data']
+        if 'rus_data' in data:
+            return 'ru'
+        elif 'lt_data' in data:
+            return 'lt'
+        return 'en'
+
+    def get_formatted_value(self, options):
+        return self.get_value(options)
+
+
+class UptrainGetter:
+
+    def get_value(self, options):
+        if options.get('initialize_body') is not None:
+            return 'yes'
+        return 'no'
+
+    def get_formatted_value(self, options):
+        return self.get_value(options)
+
+
+class FeaturesGetter:
+
+    def get_value(self, options):
+        if options['use_fbank']:
+            return 'fbank'
+        return 'wav2vec'
+
+    def get_formatted_value(self, options):
+        return self.get_value(options)
+
+
+class ModelGetter:
+
+    def get_value(self, options):
+        return options['model'].replace('_', '-')
+
+    def get_formatted_value(self, options):
+        return '\\texttt{' + self.get_value(options) + '}'
+
+
+class FloatGetter:
+
+    def __init__(self, field, frmt):
+        self._field = field
+        self._format = frmt
+
+    def get_value(self, options):
+        return options[self._field]
+
+    def get_formatted_value(self, options):
+        return self._format.format(self.get_value(options))
+
+class PossibleNoneFieldGetter:
+
+    def __init__(self, field, default_value=None, default_formatted_value=None):
+        self._field = field
+        self._default_value = default_value
+        self._default_formatted_value = default_formatted_value
+
+    def get_value(self, options):
+        return options[self._field] or self._default_value
+
+    def get_formatted_value(self, options):
+        return str(options[self._field] or self._default_formatted_value)
+
+
+GETTERS = {
+    'limit': PossibleNoneFieldGetter('limit', -1, 'no'),
+    'uptrain': UptrainGetter(),
+    'features': FeaturesGetter(),
+    'batch_size': FieldGetter('batch_size'),
+    'dev_every_batches': FieldGetter('dev_every_batches'),
+    'model': ModelGetter(),
+    'lr': FloatGetter('lr', '{:.04f}'),
+    'lr_drop': FloatGetter('lr_drop', '{:.04f}'),
+    'lang': LangGetter(),
+    'accuracy': FloatGetter('accuracy', '{:.02f}%'),
+    'xent': FloatGetter('xent', '{:.03f}')
+}
+FIELDS = ['limit', 'uptrain', 'features', 'batch_size', 'dev_every_batches', 'model', 'lr', 'lr_drop', 'lang']
+METRICS = ['accuracy', 'xent']
 
 
 def _get_fnames():
@@ -21,7 +118,8 @@ def _get_params():
 
 
 def _get_key(options, keys):
-    return tuple(options[key] or -1 for key in keys)
+    return tuple(GETTERS[key].get_value(options) for key in keys)
+
 
 def _get_lang(data):
     if 'rus_data' in data:
@@ -29,6 +127,12 @@ def _get_lang(data):
     if 'lt_data' in data:
         return 'lt'
     return 'en'
+
+
+def _get_uptrain(data):
+    if 'initialize_body' in data:
+        return 'yes'
+    return 'no'
 
 
 def _get_features(use_fbank):
@@ -51,8 +155,21 @@ def main():
             metrics = json.load(fin)
         with open(os.path.join(os.path.dirname(fname), 'options.json')) as fin:
             options = json.load(fin)
-        if any(field not in options for field in NEEDED):
-            continue
+
+        for field in FIELDS:
+            try:
+                GETTERS[field].get_value(options)
+            except Exception as ex:
+                print("Can't get value '{}' from {}".format(field, fname), file=sys.stderr)
+                continue
+
+        for metric in METRICS:
+            try:
+                GETTERS[metric].get_value(metrics)
+            except Exception as ex:
+                print("Can't get metric '{}' from {}".format(metric, fname), file=sys.stderr)
+                continue
+
         results.append((options, metrics))
     print('Loaded {} entries'.format(len(results)), file=sys.stderr)
 
@@ -67,27 +184,16 @@ def main():
 
 
     if args.header:
-        row = NEEDED + ['accuracy', 'xent']
+        row = FIELDS + METRICS
         print(' & '.join(row) + ' \\\\')
 
     for key in sorted(key2best):
         options, metrics = key2best[key]
         row = []
-        for field in NEEDED:
-            if field == 'data':
-                row.append(_get_lang(options['data']))
-            elif field == 'use_fbank':
-                row.append(_get_features(options[field]))
-            elif field == 'model':
-                row.append(_get_model(options[field]))
-            elif field in ['lr', 'lr_drop']:
-                row.append('{:.04f}'.format(options[field]))
-            elif options[field] is None:
-                row.append('no')
-            else:
-                row.append(str(options[field]))
-        row.append('{:.02f}\%'.format(metrics['accuracy']))
-        row.append('{:.03f}'.format(metrics['xent']))
+        for field in FIELDS:
+            row.append(GETTERS[field].get_formatted_value(options))
+        for metric in METRICS:
+            row.append(GETTERS[metric].get_formatted_value(metrics))
         print(' & '.join(row) + ' \\\\')
 
 
